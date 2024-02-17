c60f16019c84498fae90a4f40fdda3e71f5494dc
Hale Arslan <75578501+fhalearslnn@users.noreply.github.com>
2024-02-17 16:05:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
 terraform {
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 backend "s3" {
-    bucket         = "jenkins-project-bucket" # Replace with your S3
-    key            = "backend/jenkins-backend-jenkins.tfstate"      # Replace with your desired file name
-    region         = "us-west-1"              # Replace with your AWS Region
+  bucket = "jenkins-project-haley-backend"           # Replace with your S3
+  key    = "backend/jenkins-backend-jenkins.tfstate" # Replace with your desired file name
+  region = "us-west-1"                               # Replace with your AWS Region
 }
 provider "aws" {
   region = "us-east-1" # Replace with your AWS Region
 }
 
 variable "tags" {
-    default = ["postgres", "nodejs", "react"]
+  default = ["postgres", "nodejs", "react"]
 }
 variable "user" {
-    default = "haley"
+  default = "haley"
 }
 
 
 // Define security group for PostgreSQL
 resource "aws_security_group" "prj-sec-grp" {
-  name        = "prj-sec-grp-${var.user}"
+  name = "prj-sec-grp-${var.user}"
   tags = {
     "Name" = "prj-sec-grp"
   }
   description = "Security group for Jenkins Project"
-  
+
   // Define ingress rules to allow traffic from Node.js EC2 instance and SSH from anywhere
   ingress {
     from_port   = 5432 // postgres server
@@ -39,26 +39,26 @@ resource "aws_security_group" "prj-sec-grp" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] // Allow traffic from anywhere
   }
-  
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] // Allow SSH from anywhere
   }
-  ingress { 
-    from_port       = 5000 // nodejs server
-    to_port         = 5000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]    
-  }
   ingress {
-    from_port = 3000  // react server
-    to_port = 3000
-    protocol = "tcp"
+    from_port   = 5000 // nodejs server
+    to_port     = 5000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+  ingress {
+    from_port   = 3000 // react server
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   // Define egress rule to allow all outbound traffic
   egress {
     from_port   = 0
@@ -72,14 +72,14 @@ resource "aws_security_group" "prj-sec-grp" {
 // Define EC2 instances
 resource "aws_instance" "managed" {
   ami                    = "ami-016eb5d644c333ccb" // Example AMI, replace with your desired AMI
-  count = 3
+  count                  = 3
   instance_type          = "t2.micro"
   key_name               = "firstkey" // Replace with your key pair name
-  vpc_security_group_ids = [ aws_security_group.prj-sec-grp.id ]
-  iam_instance_profile = "jenkins-project-profile-${var.user}" // Created by Jenkins server
+  vpc_security_group_ids = [aws_security_group.prj-sec-grp.id]
+  iam_instance_profile   = "jenkins-project-profile-${var.user}" // Created by Jenkins server
   tags = {
-    Name = "ansible_${element(var.tags, count.index)}"
-    stack = "ansible_project"
+    Name        = "ansible_${element(var.tags, count.index)}"
+    stack       = "ansible_project"
     environment = "development"
   }
   user_data = <<-EOF
@@ -89,12 +89,12 @@ resource "aws_instance" "managed" {
 }
 
 output "react_ip" {
-    value = "http://${aws_instance.managed[2].public_ip}:3000/"
-  
+  value = "http://${aws_instance.managed[2].public_ip}:3000/"
+
 }
 output "public_ip_node" {
   value = aws_instance.managed[1].public_ip
 }
 output "private_ip_postgres" {
-    value = aws_instance.managed[0].private_ip
+  value = aws_instance.managed[0].private_ip
 }
